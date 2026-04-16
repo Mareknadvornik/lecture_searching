536a00fcf32a93659edff809d93587f14ab36342
261196 <261196@vutbr.cz>
2026-04-16 15:13:03 +0200

    nacteni

--- searching.py
+++ searching.py
@@ -1,26 +1,20 @@
+from csv import DictReader
 from pathlib import Path
 import json
 
 
-def read_data(file_name, field):
-    """
-    Reads a JSON file and returns data for a given field.
+def read_data(file_name, key):
+    with open("sequential.json","r") as file:
+        data=json.load(file)
+    return data
 
-    Args:
-        file_name (str): Name of the JSON file.
-        field (str): Key to retrieve from the JSON data.
-            Must be one of: 'unordered_numbers', 'ordered_numbers' or 'dna_sequence'.
 
-    Returns:
-        list | str | None:
-            - list: If data retrieved by the selected field contains numeric data.
-            - str: If field is 'dna_sequence'.
-            - None: If the field is not supported.
-    """
-    # get current working directory path
-    cwd_path = Path.cwd()
-    
-    file_path = cwd_path / file_name
+
+
+    # # get current working directory path
+    # cwd_path = Path.cwd()
+    #
+    # file_path = cwd_path / file_name
 
 
 def main():
