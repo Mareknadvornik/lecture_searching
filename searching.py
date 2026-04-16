1c0e5b9b7da2ee8615ccc9a3b9ece46bae1f48da
261196 <261196@vutbr.cz>
2026-04-16 15:15:28 +0200

    nacteni s podminkou

--- searching.py
+++ searching.py
@@ -6,7 +6,10 @@ import json
 def read_data(file_name, key):
     with open("sequential.json","r") as file:
         data=json.load(file)
-    return data
+    if key in data:
+        return data[key]
+    else:
+        return None
 
 
 
